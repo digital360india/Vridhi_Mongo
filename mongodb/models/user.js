79fe1a0b630c7e4b5812c4baf17a1e8b4b25a0e3
VridhiWtx <122273850+VridhiWtx@users.noreply.github.com>
2023-02-27 15:57:26 +0530

    setting default value to " "

--- mongodb/models/user.js
+++ mongodb/models/user.js
@@ -2,11 +2,11 @@ import mongoose from 'mongoose';
 
 
 const UserSchema = new mongoose.Schema({
-    Name: { type: String, required: true, default: "" },
-    email: { type: String, required: true, default: "" },
+    Name: { type: String, required: true, default: " " },
+    email: { type: String, required: true, default: " " },
     Phone_number: { type: String, required: true },
-    Gender: { type: String, required: true, default: "" },
-    DOB: { type: String, required: true, default: "" },
+    Gender: { type: String, required: true, default: " " },
+    DOB: { type: String, required: true, default: " " },
     No_of_Units: { type: Number, required: true, default: 0 },
     Profit: { type: Number, required: true, default: 0, ref: 'Unit' },
     Basic: { type: Number, required: true, default: 0 },
