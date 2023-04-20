8fceaefb5e518593f37a341f02276c33d102b829
VridhiWtx <Vridhidatabase@gmail.com>
2023-04-20 17:10:26 +0530

    Update user.routes.js

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -6,8 +6,8 @@ const router = express.Router();
 
 router.route('/').post(createUser);
 router.route('/').get(getAllUsers);
-router.route('/:id').get(getUserInfoByID);
 router.route('/getEnergyDashboard').get(getEnergyDashboard);
+router.route('/:id').get(getUserInfoByID);
 router.route('/:id').patch(updateUser);
 router.route('/activity/:id').patch(updateUserActivity);
 
