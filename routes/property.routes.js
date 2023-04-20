b8daa1d60a9c4a5edd36caa97cd4830b6d912c8d
VridhiWtx <Vridhidatabase@gmail.com>
2023-04-20 18:19:06 +0530

    Update property.routes.js

--- routes/property.routes.js
+++ routes/property.routes.js
@@ -7,8 +7,8 @@ const router = express.Router();
 router.route('/').post(createProperty);
 router.route('/').get(getAllProperties);
 router.route('/:id').patch(updateProperty);
-router.route('/:id').get(getPropertyInfoById);
 router.route('/propDash').get(getPropDash);
+router.route('/:id').get(getPropertyInfoById);
 router.route('/activeBids/:id').get(getActiveBids);
 
 export default router;
